4be06ec2c117a4df0eade2dee5d56b197cce6b92
zePatPat <pice287@yahoo.de>
2016-05-16 22:46:37 +0200

    cleanup

---
+++ project/Assets/Materials/Scripts/Door.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Door : MonoBehaviour {
+
+    public int scene;
+    public GameObject loadingScreen;
+
+	void OnTriggerEnter2D()
+    {
+        print("door triggered");
+        loadingScreen.SetActive(true);
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ project/Assets/Materials/Scripts/cameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float cameraDistance = -10.0f;
+		
+	void Update ()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, cameraDistance);
+	}
+}
