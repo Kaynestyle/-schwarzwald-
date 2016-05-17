b34f7fc747f923e7ead4ad06e00793fc67144547
zePatPat <pice287@yahoo.de>
2016-05-17 18:40:01 +0200

    basic combat

--- project/Assets/AI/NPC.cs
+++ project/Assets/AI/NPC.cs
@@ -1,37 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class NPC : MonoBehaviour {
 
-    public Transform target;
-    public Unit _Unit;
 
-    public int maxHealth, curHealth;
-    public gender myGender;
-    public string myName, mySurname;
-    public duty myDuty;
-    public float movSpeed = 5;
+    //variables
 
-    public enum gender {
+    private Transform target;
+    private Unit unit;
+
+    private int maxHealth, curHealth;
+    private gender myGender;
+    private string myName, mySurname;
+    private duty myDuty;
+    private float movSpeed = 5;
+
+    private damageResistance myResistance;
+
+    public enum gender
+    {
         neutral,
         male,
         female
     }
-    public enum duty {
+    public enum duty
+    {
         none,
         fisher,
         smith,
         etc
     }
 
+    int[] damageResistance = new int[3];
+
+    //functions
 
     void Awake(){
-        _Unit = this.gameObject.AddComponent<Unit>();
+        unit = this.gameObject.AddComponent<Unit>();
     }
 
     void Start(){
-        //zum Test
-        moveToTarget(target);
+        for(int i = damageResistance.Length; i>0; i--)
+        {
+            damageResistance[i] = 0;
+        }
     }
 
     void Update(){
@@ -39,7 +52,102 @@ public class NPC : MonoBehaviour {
     }
 
     void moveToTarget(Transform _target){
-        _Unit._requestPath(movSpeed, _target);
+        unit._requestPath(movSpeed, _target);
+    }
+
+
+    //getters
+
+    public Transform getTarget()
+    {
+        return target;
+    }
+
+    public int getMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public int getCurHealth()
+    {
+        return curHealth;
+    }
+
+    public gender getGender()
+    {
+        return myGender;
+    }
+
+    public string getName()
+    {
+        return myName;
+    }
+
+    public string getSurname()
+    {
+        return mySurname;
+    }
+
+    public duty getDuty()
+    {
+        return myDuty;
+    }
+
+    public float getMovSpeed()
+    {
+        return movSpeed;
+    }
+
+    public int getResistance(int _resistance)
+    {
+        return damageResistance[_resistance];
+    }
+
+    //setters
+
+    public void setTarget(Transform _target)
+    {
+        this.target = _target;
+    }
+
+    public void setMaxHealth(int _maxHealth)
+    {
+        this.maxHealth = _maxHealth;
+    }
+
+    public void setCurHealth(int _curHealth)
+    {
+        this.curHealth = _curHealth;
+    }
+
+    public void setGender(gender _gender)
+    {
+        this.myGender = _gender;
+    }
+
+    public void setName(string _name)
+    {
+        this.myName = _name;
+    }
+
+    public void getSurname(string _surname)
+    {
+        this.mySurname = _surname;
+    }
+
+    public void getDuty(duty _duty)
+    {
+        this.myDuty = _duty;
+    }
+
+    public void getMovSpeed(float _movSpeed)
+    {
+        this.movSpeed = _movSpeed;
+    }
+
+    public void setResistance(int _resistance, int _value)
+    {
+        damageResistance[_resistance] = _value;
     }
 
 }
---
+++ project/Assets/Combat/Combat_Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat_Main : MonoBehaviour {
+
+    public enum damageType
+    {
+        a,
+        b,
+        c
+    }
+
+    public void attack(Transform _target, damageType _damageType, int damageValue)
+    {
+        NPC npc = _target.gameObject.GetComponent<NPC>();
+        npc.setCurHealth(npc.getCurHealth()-(int)(1-(npc.getResistance((NPC.damageResistance)_damageType)))*damageValue);
+    }
+
+}
